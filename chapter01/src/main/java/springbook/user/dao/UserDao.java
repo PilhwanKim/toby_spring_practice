0cbd8bafc71398cff3b91302025c6658e06a2748
pilhwanKim <pilhwankim03@gmail.com>
2017-12-14 06:51:26 +0900

    1.1. 초난감 DAO
    시작과 함께, readme 파일로 중요한 내용은 요점정리

---
+++ chapter01/src/main/java/springbook/user/dao/UserDao.java
@@ -0,0 +1,67 @@
+package springbook.user.dao;
+
+import springbook.user.domain.User;
+
+import java.sql.*;
+
+/**
+ * Created by pilhwankim on 13/12/2017.
+ */
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
+
+        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
+
+        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+
+        rs.next();
+
+        User user = new User();
+
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        User user = new User();
+        user.setId("leon0517");
+        user.setName("김필환");
+        user.setPassword("secret1!");
+
+        UserDao userDao = new UserDao();
+        userDao.add(user);
+
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = userDao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + "조회 성공");
+    }
+
+}
