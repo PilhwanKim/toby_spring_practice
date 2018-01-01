dbd77a23e24375983999789916e8de6d4a285107
pilhwanKim <pilhwankim03@gmail.com>
2018-01-02 06:44:44 +0900

    1.5. 스프링의 IoC

--- chapter01/src/main/java/springbook/user/dao/DaoFactory.java
+++ chapter01/src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,9 +1,17 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-        return userDao;
+        return new UserDao(connectionMaker());
+    }
+
+    @Bean
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
     }
 }
--- chapter01/src/main/java/springbook/user/dao/UserDaoTest.java
+++ chapter01/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -1,5 +1,7 @@
 package springbook.user.dao;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
@@ -15,10 +17,11 @@ public class UserDaoTest {
         user.setName("김필환");
         user.setPassword("secret1!");
 
-        // var1. 관계 설정 코드가 클라이언트 class(UserDaoTest) 에서 하도록 변경되었다. 이제야 서로가 완전히 분리되었다.
-        // var2. 관계 설정 역할을 DaoFactory 로 분리시켰다. UserDaoTest 역할이 한가지로 줄어들었다.
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userDao = daoFactory.userDao();
+        // ver1. 관계 설정 코드가 클라이언트 class(UserDaoTest) 에서 하도록 변경되었다. 이제야 서로가 완전히 분리되었다.
+        // ver2. 관계 설정 역할을 DaoFactory 로 분리시켰다. UserDaoTest 역할이 한가지로 줄어들었다.
+        // ver3. 본격적으로 Spring 사용한다.
+        ApplicationContext ac = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao userDao = ac.getBean("userDao", UserDao.class);
 
         System.out.println(user.getId() + "등록 성공");
 
