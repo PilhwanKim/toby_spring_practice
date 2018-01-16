805cc7be51433b8c0d0de5e0fe85f1bc10ce40bc
Leon Kim <pilhwankim03@gmail.com>
2018-01-16 14:29:42 +0900

    2.3.5. 테스트 코드 개선

--- chapter02/src/test/java/springbook/user/dao/UserDaoTest.java
+++ chapter02/src/test/java/springbook/user/dao/UserDaoTest.java
@@ -1,6 +1,6 @@
 package springbook.user.dao;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -16,42 +16,45 @@ import static org.hamcrest.CoreMatchers.is;
  * Created by pilhwankim on 15/12/2017.
  */
 public class UserDaoTest {
+    private UserDao dao;
+    private User user1;
+    private User user2;
+    private User user3;
+
+    @Before
+    public void setUp() {
+        ApplicationContext ac = new GenericXmlApplicationContext("applicationContext.xml");
+        this.dao = ac.getBean("userDao", UserDao.class);
+
+        this.user1 = new User("pilhwankim", "김필환", "secret2@");
+        this.user2 = new User("leegm700", "이길원", "springno1");
+        this.user3 = new User("bumjin", "박범진", "springno2");
+    }
 
     @Test
     public void addAndGet() throws SQLException {
-        ApplicationContext ac = new GenericXmlApplicationContext("/applicationContext.xml");
-        UserDao userDao = ac.getBean("userDao", UserDao.class);
 
-        userDao.deleteAll();
-        assertThat(userDao.getCount(), is(0));
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
 
 
-        User user1 = new User("pilhwankim", "김필환", "secret2@");
-        User user2 = new User("leegm700", "이길원", "springno2");
-        userDao.add(user1);
-        userDao.add(user2);
+        dao.add(user1);
+        dao.add(user2);
 
-        assertThat(userDao.getCount(), is(2));
+        assertThat(dao.getCount(), is(2));
 
 
-        User userget1 = userDao.get(user1.getId());
+        User userget1 = dao.get(user1.getId());
         assertThat(userget1.getName(), is(user1.getName()));
         assertThat(userget1.getPassword(), is(user1.getPassword()));
 
-        User userget2 = userDao.get(user2.getId());
+        User userget2 = dao.get(user2.getId());
         assertThat(userget2.getName(), is(user2.getName()));
         assertThat(userget2.getPassword(), is(user2.getPassword()));
     }
 
     @Test
     public void count() throws SQLException {
-        ApplicationContext ac = new GenericXmlApplicationContext("/applicationContext.xml");
-        UserDao dao = ac.getBean("userDao", UserDao.class);
-
-        User user1 = new User("pilhwankim", "김필환", "secret2@");
-        User user2 = new User("leegm700", "이길원", "springno1");
-        User user3 = new User("bumjin", "박범진", "springno2");
-
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
@@ -67,9 +70,6 @@ public class UserDaoTest {
 
     @Test(expected = EmptyResultDataAccessException.class)
     public void getUserFailure() throws SQLException {
-        ApplicationContext ac = new GenericXmlApplicationContext("/applicationContext.xml");
-        UserDao dao = ac.getBean("userDao", UserDao.class);
-
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
