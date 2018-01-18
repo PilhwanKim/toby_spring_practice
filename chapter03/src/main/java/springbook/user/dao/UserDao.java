87e716854accaf08d85f1860365d6fa603df1e58
Leon Kim <pilhwankim03@gmail.com>
2018-01-18 14:48:10 +0900

    3.1. 다시보는 초난감 DAO

--- chapter03/src/main/java/springbook/user/dao/UserDao.java
+++ chapter03/src/main/java/springbook/user/dao/UserDao.java
@@ -15,68 +15,150 @@ public class UserDao {
     private DataSource dataSource;
 
     public void add(User user) throws SQLException {
-        Connection c = dataSource.getConnection();
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = dataSource.getConnection();
 
-        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
+            ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
 
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
+            ps.executeUpdate();
+        } catch(SQLException e) {
+            throw e;
+        } finally {
+            if(ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e){ }
+                /*
+                ps.close() 메소드에도 SQLException이 발생할 수 있기 때문에 이를 잡아줘야 한다.
+                그렇지 않으면 Connection close() 하지 못하고 메소드를 빠져나가게 된다.
+                */
+            }
+            if(c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e){ }
+            }
+        }
     }
 
     public User get(String id) throws SQLException {
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-        ps.setString(1, id);
-
-        ResultSet rs = ps.executeQuery();
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
         User user = null;
+        try {
+            c = dataSource.getConnection();
 
-        if (rs.next()) {
-            user = new User();
-            user.setId(rs.getString("id"));
-            user.setName(rs.getString("name"));
-            user.setPassword(rs.getString("password"));
+            ps = c.prepareStatement("select * from users where id = ?");
+            ps.setString(1, id);
 
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                user = new User();
+                user.setId(rs.getString("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+            }
+
+            if(user == null) throw new EmptyResultDataAccessException(1);
+
+            return user;
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if(rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e){ }
+                /*
+                rs.close() 메소드에도 SQLException이 발생할 수 있기 때문에 이를 잡아줘야 한다.
+                그렇지 않으면 Connection close() 하지 못하고 메소드를 빠져나가게 된다.
+                */
+            }
+            if(ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e){ }
+            }
+            if(c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e){ }
+            }
         }
 
-        rs.close();
-        ps.close();
-        c.close();
-
-        if(user == null) throw new EmptyResultDataAccessException(1);
-
-        return user;
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = dataSource.getConnection();
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("delete from users");
+            ps.executeUpdate();
+        } catch (SQLException e){
+            throw e;
+        } finally {
+            if(ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e){ }
+                /*
+                ps.close() 메소드에도 SQLException이 발생할 수 있기 때문에 이를 잡아줘야 한다.
+                그렇지 않으면 Connection close() 하지 못하고 메소드를 빠져나가게 된다.
+                */
+            }
+            if(c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e){ }
+            }
+        }
 
-        PreparedStatement ps = c.prepareStatement("delete from users");
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
     }
 
     public int getCount() throws SQLException {
-        Connection c = dataSource.getConnection();
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
-        PreparedStatement ps = c.prepareStatement("select count(*) from users");
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
+        try {
+            c = dataSource.getConnection();
 
-        rs.close();
-        ps.close();
-        c.close();
+            ps = c.prepareStatement("select count(*) from users");
 
-        return count;
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if(rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e){ }
+                /*
+                rs.close() 메소드에도 SQLException이 발생할 수 있기 때문에 이를 잡아줘야 한다.
+                그렇지 않으면 Connection close() 하지 못하고 메소드를 빠져나가게 된다.
+                */
+            }
+            if(ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e){ }
+            }
+            if(c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e){ }
+            }
+        }
     }
 
     public void setDataSource(DataSource dataSource) { this.dataSource = dataSource; }
