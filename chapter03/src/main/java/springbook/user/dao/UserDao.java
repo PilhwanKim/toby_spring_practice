91bb5de914a4d0ac44815261817103d1617e52d7
Leon Kim <pilhwankim03@gmail.com>
2018-01-22 10:59:57 +0900

    3.4.2. JdbcContext의 특별한 DI - 코드를 이용하는 수동 DI

--- chapter03/src/main/java/springbook/user/dao/UserDao.java
+++ chapter03/src/main/java/springbook/user/dao/UserDao.java
@@ -128,9 +128,9 @@ public class UserDao {
         }
     }
 
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcContext = new JdbcContext();
+        this.jdbcContext.setDataSource(dataSource);
+        this.dataSource = dataSource;
     }
-
-    public void setDataSource(DataSource dataSource) { this.dataSource = dataSource; }
 }
